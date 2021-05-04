0f9f8a628bac47f11ef1f7cd0942de618e8cbd14
Nurina <nurina.junus@gmail.com>
2021-05-03 22:25:41 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+// Grab dodger
+const dodger = document.getElementById("dodger"); 
+dodger.style.backgroundColor = "#FF69B4"; 
+dodger.style.bottom = "0px"; 
+
+// initiate moveDodgerLeft() function
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`;
+    }
+  } 
+
+// initiate moveDodgerRight()
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 10}px`;
+    }
+}
+
+// associate moveDodgerLeft() with eventListener
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  }); 
