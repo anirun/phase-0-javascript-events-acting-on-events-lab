8a3a5267c316307ac6140133f6450b4b5efc3e63
Nurina <nurina.junus@gmail.com>
2021-06-13 21:20:17 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-// Grab dodger
-const dodger = document.getElementById("dodger"); 
+// grab dodger
+const dodger = document.getElementById("dodger");
+
+// change dodger color
 dodger.style.backgroundColor = "#FF69B4"; 
+
+// set start point
 dodger.style.bottom = "0px"; 
 
-// initiate moveDodgerLeft() function
+// moveleft fxn
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 10}px`;
-    }
-  } 
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-// initiate moveDodgerRight()
+  if (left > 0) {
+    dodger.style.left = `${left - 3}px`;
+  }
+} 
+
+// moveRight fxn
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-    if (left < 360) {
-        dodger.style.left = `${left + 10}px`;
-    }
-}
+  if (left > 0) {
+    dodger.style.left = `${left + 3}px`;
+  }
+} 
 
-// associate moveDodgerLeft() with eventListener
+// move dodger left when left pressed
 document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-    if (e.key === "ArrowRight") {
-      moveDodgerRight();
-    }
-  }); 
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+}); 
